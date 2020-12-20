152e25bd902b448c4de79896e2fd24f594cd9c83
Skiveeee <76391030+Skiveeee@users.noreply.github.com>
2020-12-20 23:07:11 +0200

    added home view (#1)
    Co-authored-by: Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:skive/views/home.view.dart';
 
 void main() {
@@ -15,6 +16,24 @@ class Skive extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeView(),
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        return ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, child),
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(920, name: TABLET),
+            ResponsiveBreakpoint.autoScale(1200, name: DESKTOP),
+            ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(
+            color: Color(0xFFF5F5F5),
+          ),
+        );
+      },
     );
   }
 }
