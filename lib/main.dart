9085ed64f1b58f8f2503660503b6b61729a809c1
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2020-12-20 13:10:59 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,11 @@ class Skive extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Skive',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomeView(),
-    );
+        title: 'Skive',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeView());
   }
 }
